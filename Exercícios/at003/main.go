9f43e8dcb7c1b45091894cc4b7bf33cbe58634ee
Mayko Souza <maykosouzadev@gmail.com>
2026-01-15 22:58:57 -0300

    feat: :seedling: Exercício

--- Exercícios/at003/main.go
+++ Exercícios/at003/main.go
@@ -13,15 +13,23 @@ type server struct {
 	Status bool `json:"status"`
 
 }
+var servidores []server
 
 func main() {
 	leitorDoArquivo()
 }
 
 func leitorDoArquivo() {
-	arquivo, err := os.ReadFile("server.json")
+	arquivo, err := os.ReadFile("servidores.json")
 	if err != nil {
 		fmt.Println("Erro ao ler o arquivo:", err)
+		return
 	}
-	
+
+	err = json.Unmarshal(arquivo, &servidores)
+	if err != nil {
+		fmt.Println("Erro ao fazer o unmarshal:", err)
+		return
+	}
+	fmt.Printf("Servidores lidos: %v\n", servidores)
 }
